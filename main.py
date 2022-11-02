d474071557e96984c998d6131425e393cd89aaa9
griforey86 <griforey86@gmail.com>
2022-11-03 02:07:00 +0500

    суммаризатор

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from speechToText import func_speech
 from boto_file import uploadToBucketAndGetPath
-from summariozation import summarization
+from summarization import summarization_sbercloud
+from summarization import summarization_spacy
 import streamlit as st
 import tempfile
 
@@ -13,14 +14,16 @@ if file is not None:
     temp = tempfile.NamedTemporaryFile(mode="wb")
     bytes_data = file.getvalue()
     temp.write(bytes_data)
-    obj_response = uploadToBucketAndGetPath('itis',temp.name)
+    obj_response = uploadToBucketAndGetPath('itis', temp.name)
 
     resultText = func_speech(obj_response)
     st.text(resultText)
 
-    resultSummarization = summarization(resultText)
+    resultSummarizationSbercloud = summarization_sbercloud(resultText)
+    resultSummarizationSpacy = summarization_spacy(resultText, 5)
 
-    st.text(str(resultSummarization['predictions']))
+    st.text(str(resultSummarizationSbercloud['predictions']))
+    st.text(str(resultSummarizationSpacy))
 
 
     
---
+++ summarization.py
@@ -0,0 +1,61 @@
+import requests
+import json
+import spacy
+from collections import Counter
+from string import punctuation
+
+
+def summarization_sbercloud(text):
+    url = "https://api.aicloud.sbercloud.ru/public/v2/summarizator/predict"
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    }
+    data = {
+        'instances': [
+            {
+                'text': text,
+                'num_return_sequences': 100,
+                'length_penalty': 10
+            }
+        ]
+    }
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return json.loads(response.text)
+
+
+def summarization_spacy(text, limitSentences=0):
+    nlp = spacy.load("ru_core_news_lg")
+    # токенизация
+    keywords = []
+    tags = ['PROPN', 'ADJ', 'NOUN', 'VERB']
+    doc = nlp(text.lower().replace('\n', '').replace('. ', '.').replace('.', '. '))
+    for token in doc:
+        if (token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        if (token.pos_ in tags):
+            keywords.append(token.text)
+    # нормализация частоты слов
+    word_freq = Counter(keywords)
+    max_freq = Counter(keywords).most_common(1)[0][1]
+    for w in word_freq:
+        word_freq[w] = (word_freq[w] / max_freq)
+    # важность предложений
+    sent_power = {}
+    for sent in doc.sents:
+        for word in sent:
+            if word.text in word_freq.keys():
+                if sent in sent_power.keys():
+                    sent_power[sent] += word_freq[word.text]
+                else:
+                    sent_power[sent] = word_freq[word.text]
+    summary = []
+    sorted_x = sorted(sent_power.items(), key=lambda kv: kv[1], reverse=True)
+    counter = 0
+    for i in range(len(sorted_x)):
+        summary.append(str(sorted_x[i][0]).capitalize())
+
+        counter += 1
+        if (counter >= limitSentences):
+            break
+    return ' '.join(summary)
