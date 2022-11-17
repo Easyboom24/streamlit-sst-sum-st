2d1c826d0e0b659f2d7a53caba91a7b7b6508fc0
griforey86 <griforey86@gmail.com>
2022-11-17 15:04:45 +0500

    аналитика

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from speechToText import func_speech
 from boto_file import uploadToBucketAndGetPath
 import streamlit as st
-import streamlit.components.v1 as components
+import streamlit_analytics as sta
+# import streamlit.components.v1 as components
 import tempfile
 #from summarization import summarization_sbercloud
 
@@ -10,43 +11,43 @@ from annotation import get_annotation
 #import os
 #os.system("python -m spacy download ru_core_news_lg")
 
-components.html("""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-CH2M532WGY"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+# components.html("""
+# <!-- Google tag (gtag.js) -->
+# <script async src="https://www.googletagmanager.com/gtag/js?id=G-CH2M532WGY"></script>
+# <script>
+#   window.dataLayer = window.dataLayer || [];
+#   function gtag(){dataLayer.push(arguments);}
+#   gtag('js', new Date());
+#
+#   gtag('config', 'G-CH2M532WGY');
+# </script>
+# """)
 
-  gtag('config', 'G-CH2M532WGY');
-</script>
-""")
+with sta.track():
+    percent_of_text_sum = st.slider(label="Процент сокращения текста", min_value=0, max_value=100, value=50)
+    file = st.file_uploader(label="Загрузите аудиозапись")
 
+    st.header('Выберите, что вы хотите выделить в тексте:')
+    names = st.checkbox('Личности, имена 🔴')
+    orgs = st.checkbox('Компании, организации 🟡')
+    locs = st.checkbox('Места, локации 🔵')
+    money = st.checkbox('Деньги, валюта 🟢')
+    dates = st.checkbox('Даты 🟣')
 
-percent_of_text_sum = st.slider(label="Процент сокращения текста", min_value=0, max_value=100, value=50)
-file = st.file_uploader(label="Загрузите аудиозапись")
+    if file is not None:
+        temp = tempfile.NamedTemporaryFile(mode="wb")
+        bytes_data = file.getvalue()
+        temp.write(bytes_data)
+        obj_response = uploadToBucketAndGetPath('itis', temp.name)
 
-st.header('Выберите, что вы хотите выделить в тексте:')
-names = st.checkbox('Личности, имена 🔴')
-orgs = st.checkbox('Компании, организации 🟡')
-locs = st.checkbox('Места, локации 🔵')
-money = st.checkbox('Деньги, валюта 🟢')
-dates = st.checkbox('Даты 🟣')
+        st.header("Исходный текст")
+        resultText = func_speech(obj_response)
+        st.write(resultText)
 
-if file is not None:
-    temp = tempfile.NamedTemporaryFile(mode="wb")
-    bytes_data = file.getvalue()
-    temp.write(bytes_data)
-    obj_response = uploadToBucketAndGetPath('itis', temp.name)
+        st.header("Сокращенный текст")
+        resultSummarizationSpacy = summarization_spacy(resultText, percent_of_text_sum)
+        st.write(str(resultSummarizationSpacy))
 
-    st.header("Исходный текст")
-    resultText = func_speech(obj_response)
-    st.write(resultText)
-
-    st.header("Сокращенный текст")
-    resultSummarizationSpacy = summarization_spacy(resultText, percent_of_text_sum)
-    st.write(str(resultSummarizationSpacy))
-    
-    st.header("Текст с выделенными фрагментами")
-    resultAnnotation = get_annotation(str(resultSummarizationSpacy), names, orgs, locs, money, dates)
-    st.markdown(resultAnnotation, unsafe_allow_html=True) 
+        st.header("Текст с выделенными фрагментами")
+        resultAnnotation = get_annotation(str(resultSummarizationSpacy), names, orgs, locs, money, dates)
+        st.markdown(resultAnnotation, unsafe_allow_html=True)
