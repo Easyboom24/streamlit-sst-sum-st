5c3e4a3dd18bd9514497eb6bd8da438d79d2f8ea
SanCheetos <94627584+SanCheetos@users.noreply.github.com>
2022-12-08 11:39:57 +0500

    Update FireBase.py

--- FireBase.py
+++ FireBase.py
@@ -1,19 +1,12 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-import streamlit as st
-import json
-import tempfile
-
+ 
 #Функция должна срабатывать каждый раз, когда нажалась кнопка и пошел процесс основной работы
 def FireBase_Push(date, percentSum, textLength, CheckBoxes, timeYandex):
-    my_str = st.secrets["KEY_FIREBASE"]
-    import json,tempfile
-    tfile = tempfile.NamedTemporaryFile(mode="w+")
-    json.dump(my_str, tfile)
-    tfile.flush()
+    cred = credentials.Certificate("key.json")
     try:
-        firebase_admin.initialize_app(tfile.name)
+        firebase_admin.initialize_app(cred)
     except:
         print("Already connected")
     ref = db.reference(path="/Analitics", url="https://streamlit-sst-sum-default-rtdb.firebaseio.com") 
@@ -34,13 +27,10 @@ def FireBase_Push(date, percentSum, textLength, CheckBoxes, timeYandex):
                  })
         return "Success"
 def FireBase_Get():
-    my_str = st.secrets["KEY_FIREBASE"]
-    import json,tempfile
-    tfile = tempfile.NamedTemporaryFile(mode="w+")
-    json.dump(my_str, tfile)
-    tfile.flush()
-    firebase_admin.initialize_app(tfile.name)
+    cred = credentials.Certificate("key.json")
+    try:
+        firebase_admin.initialize_app(cred)
+    except:
+        print("Already connected")
     ref = db.reference(path="/Analitics", url="https://streamlit-sst-sum-default-rtdb.firebaseio.com") 
     return ref.get()
-
-#print(FireBase_Push("01.09.2022", 10, 200, [True, True, False, True, True], 7))
